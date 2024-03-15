204e4b6ebdad56f84d1fe75c4899a48893f7e6f7
MichaelLeo-code <michael.leo.dk@gmail.com>
2024-03-15 16:26:57 +0100

    database setting up 1/2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,17 @@
 import express, { Request, Response} from 'express'
 import http from 'http'
 import bodyParser from 'body-parser'
+import "reflect-metadata"
 // import cookieParser from 'cookie-parser'
 // import compression from 'compression'
 // import cors from 'cors'
 
 const app = express()
-app.use(bodyParser.json())
+app.use(express.json())
 
 const server = http.createServer(app)
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req, res) => {
     return res.send("hi there")
 })
 
